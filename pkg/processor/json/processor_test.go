4980f4d42f2a98a03404c290dcbf33b10f40b0f0
vmydillic <vladyslav@idyllicvision.com>
2021-10-17 13:01:40 +0300

    add unit tests

--- pkg/processor/json/processor_test.go
+++ pkg/processor/json/processor_test.go
@@ -7,7 +7,11 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
+
+	commonJSON "encoding/json"
 )
+
+const url = "https://raw.githubusercontent.com/vmidyllic/iden3vocab/main/schemas/json/KYCAgeCredential-2.json"
 
 func TestInit(t *testing.T) {
 
@@ -22,5 +26,110 @@ func TestInit(t *testing.T) {
 
 	notDefinedError := errors.Is(errLoaderNotDefined, err)
 	assert.Equal(t, false, notDefinedError)
+
+}
+
+func TestLoader(t *testing.T) {
+
+	loader := loaders.HTTP{}
+	validator := json.Validator{}
+	parser := json.Parser{}
+
+	jsonProcessor := New(processor.WithValidator(validator), processor.WithParser(parser), processor.WithSchemaLoader(loader))
+
+	schema, ext, err := jsonProcessor.Load(url)
+
+	assert.Nil(t, err)
+	assert.Equal(t, ext, "json")
+	assert.NotEmpty(t, schema)
+}
+
+func TestValidator(t *testing.T) {
+
+	loader := loaders.HTTP{}
+	validator := json.Validator{}
+	parser := json.Parser{}
+
+	jsonProcessor := New(processor.WithValidator(validator), processor.WithParser(parser), processor.WithSchemaLoader(loader))
+
+	schema, ext, err := jsonProcessor.Load(url)
+
+	assert.Nil(t, err)
+	assert.Equal(t, ext, "json")
+	assert.NotEmpty(t, schema)
+
+	data := make(map[string]interface{})
+	data["birthdayDay"] = 24
+	data["birthdayMonth"] = 4
+	data["birthdayYear"] = 1996
+
+	dataBytes, err := commonJSON.Marshal(data)
+	assert.Nil(t, err)
+
+	err = jsonProcessor.ValidateData(dataBytes, schema)
+
+	assert.Nil(t, err)
+
+}
+
+func TestValidatorWithInvalidField(t *testing.T) {
+
+	loader := loaders.HTTP{}
+	validator := json.Validator{}
+	parser := json.Parser{}
+
+	jsonProcessor := New(processor.WithValidator(validator), processor.WithParser(parser), processor.WithSchemaLoader(loader))
+
+	schema, ext, err := jsonProcessor.Load(url)
+
+	assert.Nil(t, err)
+	assert.Equal(t, ext, "json")
+	assert.NotEmpty(t, schema)
+
+	data := make(map[string]interface{})
+	data["birthdayDay"] = 24
+	data["birthdayMonth"] = 4
+
+	dataBytes, err := commonJSON.Marshal(data)
+	assert.Nil(t, err)
+
+	err = jsonProcessor.ValidateData(dataBytes, schema)
+
+	assert.NotNil(t, err)
+	assert.Containsf(t, err.Error(), "\"birthdayYear\" value is required", "expected error containing %q, got %s", "\"birthdayYear\" value is required", err)
+
+}
+
+func TestParser(t *testing.T) {
+
+	loader := loaders.HTTP{}
+	validator := json.Validator{}
+	parser := json.Parser{}
+
+	jsonProcessor := New(processor.WithValidator(validator), processor.WithParser(parser), processor.WithSchemaLoader(loader))
+
+	schema, ext, err := jsonProcessor.Load(url)
+
+	assert.Nil(t, err)
+	assert.Equal(t, ext, "json")
+	assert.NotEmpty(t, schema)
+
+	data := make(map[string]interface{})
+	data["birthdayDay"] = 24
+	data["birthdayMonth"] = 4
+	data["birthdayYear"] = 1996
+
+	dataBytes, err := commonJSON.Marshal(data)
+	assert.Nil(t, err)
+
+	err = jsonProcessor.ValidateData(dataBytes, schema)
+
+	assert.Nil(t, err)
+
+	index, value, err := jsonProcessor.ParseSlots(dataBytes, schema)
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, index)
+	assert.Empty(t, value)
 
 }
