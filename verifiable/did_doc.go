bd964cb8f0e4d9356fcf32f4755904aa0e344b3d
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-07-25 16:30:42 +0300

    add push service

--- verifiable/did_doc.go
+++ verifiable/did_doc.go
@@ -2,19 +2,38 @@ package verifiable
 
 // DIDDocument defines current supported did doc model.
 type DIDDocument struct {
-	Context []string  `json:"@context"`
-	ID      string    `json:"id"`
-	Service []Service `json:"service"`
+	Context []string      `json:"@context"`
+	ID      string        `json:"id"`
+	Service []interface{} `json:"service"`
 }
 
-// Service DID document service field.
+// Service describes standard DID document service field.
 type Service struct {
 	ID              string `json:"id"`
 	Type            string `json:"type"`
 	ServiceEndpoint string `json:"serviceEndpoint"`
 }
 
+type PushService struct {
+	Service
+	Metadata PushMetadata `json:"metadata"`
+}
+type PushMetadata struct {
+	Devices []EncryptedDeviceMetadata `json:"devices"`
+}
+type EncryptedDeviceMetadata struct {
+	Ciphertext string `json:"ciphertext"` // base64 encoded
+	Alg        string `json:"alg"`
+}
+type DeviceMetadata struct {
+	AppID     string `json:"app_id"`
+	PushToken string `json:"push_token"`
+}
+
 const (
 	// Iden3CommServiceType is service type for iden3comm protocol
 	Iden3CommServiceType = "iden3-communication"
+
+	// PushNotificationServiceType is service type for delivering push notifications to identity
+	PushNotificationServiceType = "push-notification"
 )
