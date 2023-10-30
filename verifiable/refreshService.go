dfba976b1b0422f97fa632b4b1f8929585b45acd
Ilya <officalbatya@gmail.com>
2023-10-30 18:47:47 +0200

    add custom type for refresh service

--- verifiable/refreshService.go
+++ verifiable/refreshService.go
@@ -3,11 +3,6 @@ package verifiable
 // RefreshServiceType represent refresh service types
 type RefreshServiceType string
 
-const (
-	// ManualRefreshService2018 is the type of refresh service
-	ManualRefreshService2018 RefreshServiceType = "ManualRefreshService2018"
-)
-
 // RefreshService is struct that represents refresh service json-ld document
 type RefreshService struct {
 	ID   string             `json:"id"`
