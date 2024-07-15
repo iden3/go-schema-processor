8126039d682eb268e2f44e99e50bdbcb3648d4af
daveroga <daveroga@gmail.com>
2024-07-15 12:09:27 +0200

    add signature fields to state and gist info

--- verifiable/did_doc.go
+++ verifiable/did_doc.go
@@ -122,6 +122,7 @@ type StateInfo struct {
 	ReplacedAtTimestamp string `json:"replacedAtTimestamp"`
 	CreatedAtBlock      string `json:"createdAtBlock"`
 	ReplacedAtBlock     string `json:"replacedAtBlock"`
+	Signature           string `json:"signature"`
 }
 
 // GistInfo representation state of gist root.
@@ -132,6 +133,7 @@ type GistInfo struct {
 	ReplacedAtTimestamp string `json:"replacedAtTimestamp"`
 	CreatedAtBlock      string `json:"createdAtBlock"`
 	ReplacedAtBlock     string `json:"replacedAtBlock"`
+	Signature           string `json:"signature"`
 }
 
 // IdentityState representation all info about identity.
