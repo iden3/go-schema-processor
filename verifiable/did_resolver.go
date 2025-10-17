ba72b2424fa107218509e3c58936a780765b88d2
ilya-korotya <officalbatya@gmail.com>
2025-10-17 14:37:05 +0200

    add the consturctor to create http resolver instance

--- verifiable/did_resolver.go
+++ verifiable/did_resolver.go
@@ -12,15 +12,41 @@ import (
 	"github.com/pkg/errors"
 )
 
+// DIDResolver defines the interface for resolving DIDs to DID Documents.
 type DIDResolver interface {
 	Resolve(ctx context.Context, did *w3c.DID) (DIDDocument, error)
 }
 
+// HTTPDIDResolver resolves DIDs using an HTTP-based DID resolver.
 type HTTPDIDResolver struct {
 	resolverURL      string
 	customHTTPClient *http.Client
 }
 
+// NewHTTPDIDResolver creates a new HTTPDIDResolver with the given resolver URL and options.
+type HTTPDIDResolverOption func(*HTTPDIDResolver)
+
+// WithHTTPClient sets a custom HTTP client for the HTTPDIDResolver.
+func WithHTTPClient(client *http.Client) HTTPDIDResolverOption {
+	return func(r *HTTPDIDResolver) {
+		r.customHTTPClient = client
+	}
+}
+
+// NewHTTPDIDResolver creates a new HTTPDIDResolver with the given resolver URL and options.
+func NewHTTPDIDResolver(resolverURL string, opts ...HTTPDIDResolverOption) *HTTPDIDResolver {
+	resolver := &HTTPDIDResolver{
+		resolverURL: resolverURL,
+	}
+
+	for _, opt := range opts {
+		opt(resolver)
+	}
+
+	return resolver
+}
+
+// Resolve resolves the DID Document for the given DID using HTTP.
 func (r HTTPDIDResolver) Resolve(ctx context.Context, did *w3c.DID) (out DIDDocument, err error) {
 	type didResolutionResult struct {
 		DIDDocument DIDDocument `json:"didDocument"`
